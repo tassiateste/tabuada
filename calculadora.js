5591f1ce182d55b4105a0d8974eb377b88ead7f0
tassiateste <desenvolveta@gmail.com>
2024-07-10 19:56:43 -0300

    Código inicial

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+let numero = prompt("Digite o número da tabuada:");
+
+let contador = 1;
+
+while (contador <= 10) {
+
+    let multiplicacao = numero * contador;
+
+    console.log(`${numero} x ${contador} = ${multiplicacao}`);
+
+    contador++;
+    
+}
